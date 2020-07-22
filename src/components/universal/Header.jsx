1aac3d099b2864cc6fd8bd790c3e4524fef99eac
Mark <wowmarkambro@gmail.com>
2020-07-22 18:42:21 -0500

    minor fixes to get hosting working

--- src/components/universal/Header.jsx
+++ src/components/universal/Header.jsx
@@ -1,36 +1,28 @@
-import React from 'react';
-import {
-	Collapse,
-	Navbar,
-	NavbarToggler,
-	NavbarBrand,
-	Nav,
-	NavItem,
-	NavLink,
-} from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navbar } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-	return (
-		<Navbar className='navbar'>
-			<div className='navTitle'>
-				<i className='fas fa-truck'></i>
-				<h1 className='headerTitle'>Food Truck Trackr</h1>
-			</div>
-			<Link to='/' className='navItem'>
-				Home
-			</Link>
-			<Link to='/profile' className='navItem'>
-				Profile
-			</Link>
-			<Link to='/trucks' className='navItem'>
-				Trucks
-			</Link>
-			<Link to='/signin' className='navItem'>
-				Login/Sign Up
-			</Link>
-		</Navbar>
-	);
+    return (
+        <Navbar className='navbar'>
+            <div className='navTitle'>
+                <i className='fas fa-truck'></i>
+                <h1 className='headerTitle'>Food Truck Trackr</h1>
+            </div>
+            <Link to='/' className='navItem'>
+                Home
+            </Link>
+            <Link to='/profile' className='navItem'>
+                Profile
+            </Link>
+            <Link to='/trucks' className='navItem'>
+                Trucks
+            </Link>
+            <Link to='/signin' className='navItem'>
+                Login/Sign Up
+            </Link>
+        </Navbar>
+    );
 };
 
 export default Header;
