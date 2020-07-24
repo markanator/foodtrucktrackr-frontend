27dba660603edfd92ccb1046f7581a91e789fc1a
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-07-23 21:29:26 -0400

    set up some axios requests in the actions, started setting up the dinerOperatorReducer so that it returns the correct state

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,34 +24,75 @@ export const SEARCH_TRUCK_FAILURE = "SEARCH_TRUCK_FAILURE";
 
 export const deleteFavTruck = truck => dispatch => {
     console.log("deleteFavTruck action creator");
-    dispatch({ type: DELETE_FAV_START });
+    dispatch({ type: DELETE_FAV_START, payload: truck });
+    /* axios
+        .delete('url', truck?)
+        .then(res => {
+            console.log(res);
+            dispatch({ type: DELETE_FAV_SUCCESS });
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({ type: DELETE_FAV_FAILURE, payload: err });
+        }) */
 };
 
 export const addFavTruck = newTruck => dispatch => {
     console.log("addFavTruck action creator");
     dispatch({ type: ADD_FAV_START, payload: newTruck });
+    /* axios
+        .post('url', newTruck)
+        .then(res => {
+            console.log(res);
+            dispatch({ type: ADD_FAV_SUCCESS });
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({ type: ADD_FAV_FAILURE, payload: err });
+        }) */
 };
 
 export const editTruckRating = newRating => dispatch => {
     console.log("editTruckRating action creator");
     dispatch({ type: EDIT_RATING_START, payload: newRating});
+    /* axios
+        .put('url', newRating)
+        .then(res => {
+            console.log(res);
+            dispatch({ type: EDIT_RATING_SUCCESS });
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({ type: EDIT_RATING_FAILURE, payload: err });
+        }) */
 };
 
 export const rateTruck = rating => dispatch => {
     console.log("rateTruck action creator");
     dispatch({ type: RATE_TRUCK_START, payload: rating });
+    /* axios
+        .post('url', rating)
+        .then(res => {
+            console.log(res);
+            dispatch({ type: RATE_TRUCK_SUCCESS });
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({ type: RATE_TRUCK_FAILURE, payload: err });
+        }) */
 }
 
 export const searchTrucksStart = searchState => dispatch => {
     console.log("searchTrucksStart action creator");
     dispatch({ type: SEARCH_TRUCKS_START });
     //axios get request (may need to filter results to return what we want)
-    //axios
-        //.get("url")
-        //.then(res => { 
-            //filter results; dispatch({type: SEARCH_TRUCK_SUCCESS, payload: res....})
-        //})
-        //.catch(err => {
-            //dispatch({type: SEARCH_TRUCK_FAILURE, payload: err})
-        //});
+    /* axios
+        .get("url")
+        .then(res => { 
+            filter results; 
+            dispatch({type: SEARCH_TRUCK_SUCCESS, payload: res....})
+        })
+        .catch(err => {
+            dispatch({type: SEARCH_TRUCK_FAILURE, payload: err})
+        }); */
 };
--- src/reducers/dinerOperatorReducer.js
+++ src/reducers/dinerOperatorReducer.js
@@ -5,7 +5,9 @@ const initialState = {
         username: '',
         password: '',
         currentLocation: '',
-        favoriteTrucks: []
+        favoriteTrucks: [],
+        isAdding: false,
+        error: ""
     },
     operator: {
         username: '',
@@ -16,15 +18,51 @@ const initialState = {
 
 export const dinerOperatorReducer = (state = initialState, action) => {
     switch (action.type) {
+        // add truck to favorites
         case (actions.ADD_FAV_START):
-            return state;
+            return {
+                ...state,
+                isAdding: true
+            };
+        case (actions.ADD_FAV_SUCCESS):
+            return {
+                ...state,
+                isAdding: false,
+                favoriteTrucks: [...state.favoriteTrucks, action.payload]
+            };
+        case (actions.ADD_FAV_FAILURE):
+            return {
+                ...state,
+                isAdding: false,
+                error: action.payload
+            };
+        // edit rating
         case (actions.EDIT_RATING_START):
             return state;
+        case (actions.EDIT_RATING_SUCCESS):
+            return state;
+        case (actions.EDIT_RATING_FAILURE):
+            return state;
+        // delete truck from favorites
         case (actions.DELETE_FAV_START):
             return state;
+        case (actions.DELETE_FAV_SUCCESS):
+            return state;
+        case (actions.DELETE_FAV_FAILURE):
+            return state;
+        // rate truck
         case(actions.RATE_TRUCK_START):
             return state;
+        case(actions.RATE_TRUCK_SUCCESS):
+            return state;
+        case(actions.RATE_TRUCK_FAILURE):
+            return state;
+        // search for a truck
         case (actions.SEARCH_TRUCKS_START):
+            return state;
+        case (actions.SEARCH_TRUCKS_SUCCESS):
+            return state;
+        case (actions.SEARCH_TRUCKS_FAILURE):
             return state;
         default:
             return state;
