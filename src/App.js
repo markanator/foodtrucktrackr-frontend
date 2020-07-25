6ed40f09fd8b09f55d1f0e68b712cc82e61a83ec
Mark <wowmarkambro@gmail.com>
2020-07-24 22:37:50 -0500

    added routes, fixed css

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import "./App.css";
-import Home from './components/Home';
-import Footer from './components/universal/Footer';
-import Header from './components/universal/Header';
-import DinerDashboard from './components/DinerDashboard';
-import OperatorDashboard from './components/OperatorDashboard';
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+
+// layout
+import Layout from "./Layout";
+// local components
+import Home from "./components/Home";
+import DinerDashboard from "./components/DinerDashboard";
+import OperatorDashboard from "./components/OperatorDashboard";
+import TruckList from "./components/TruckList";
 import CreateTruckForm from "./components/CreateTruckForm";
 import TruckDetails from "./components/truck_details/TruckDetails";
 
 const App = () => {
-	const [users, setUsers] = useState([]);
-	return (
-		<div>
-			<Header />
-			<div className='App'>
-				<Switch>
-					<Route exact path='/'>
-						<Home users={users} setUsers={setUsers} />
-					</Route>
-					<Route path='/login'></Route>
-					<Route path='/operatordashboard'>
-						<OperatorDashboard />
-					</Route>
-					<Route path='/dinerdashboard'>
-						<DinerDashboard />
-					</Route>
-          <Route path="/add-truck">
-              <CreateTruckForm/>
-          </Route>
-          <Route path="/trucks/:id">
-              <TruckDetails/>
-          </Route>
-				</Switch>
-			</div>
-			<Footer />
-		</div>
-	);
+    const [users, setUsers] = useState([]);
+    return (
+        <Layout>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/">
+                        <Home users={users} setUsers={setUsers} />
+                    </Route>
+                    <Route path="/login"></Route>
+                    <Route path="/operator">
+                        <OperatorDashboard />
+                    </Route>
+                    <Route path="/profile">
+                        <DinerDashboard />
+                    </Route>
+                    <Route path="/trucks">
+                        <TruckList />
+                    </Route>
+                    <Route path="/add-truck">
+                        <CreateTruckForm />
+                    </Route>
+                    <Route path="/trucks/:id">
+                        <TruckDetails />
+                    </Route>
+                </Switch>
+            </div>
+        </Layout>
+    );
 };
 
 export default App;
