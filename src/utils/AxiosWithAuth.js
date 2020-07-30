146c81ce8845a532f0df7f830f9638e8b090af96
Mark <wowmarkambro@gmail.com>
2020-07-29 21:08:53 -0500

    revert to localhost, signup working

--- src/utils/AxiosWithAuth.js
+++ src/utils/AxiosWithAuth.js
@@ -8,7 +8,8 @@ export const axiosWithAuth = () => {
         // base url for our local server
         // once server is hosted use:
         // https://cors-anywhere.herokuapp.com/
-        baseURL: "https://foodtrackertcr.herokuapp.com",
+        // baseURL: "https://foodtrackertcr.herokuapp.com",
+        baseURL: "http://localhost:5000",
         headers: {
             Authorization: token,
         },
